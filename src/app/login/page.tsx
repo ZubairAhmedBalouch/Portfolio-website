689e154c51f4a27a9025c2f34e0cd3df278c9ac7
Zubair Ahmed Balouch <zubairahmedbalouch37@gmail.com>
2024-11-07 17:47:34 +0500

    Initial commit

---
+++ src/app/login/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import LoginPage from '../Components/login';
+import Navbar from '../Components/navbar';
+
+const page = () => {
+  return (
+    <div>
+       <Navbar />
+      <LoginPage />
+    </div>
+  )
+}
+
+export default page
